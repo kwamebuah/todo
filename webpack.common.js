50df818994014bdcb54d6feb9c750ea7388cbd37
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-04-08 13:12:25 +0000

    Set up necessary bundler dependencies
    Install necessary webpack modules
    Create necessary config files for development and production
    Create basic source files
    Create .gitignore file

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: './src/template.html',
+    }),
+  ],
+};
