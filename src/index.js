2bd1e51764d9c2655679cb183ab9484ae842d2dd
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-04-10 09:08:15 +0000

    Create function to store projects
    Make function to hold all projects in to do app.
    Function should hold a default project.
    Assign todo's to user-selected projects.

--- src/index.js
+++ src/index.js
@@ -11,15 +11,39 @@ function makeTodo() {
     return todo;
 }
 
+function storeProject(details) {
+    const todo = details.todo;
+    let projectName = details.projectName;
+    const projects = [];
+    const project1 = {
+        name: 'default',
+        tasks: [],
+    }
+
+    projects.push(project1);
+
+    if (projectName === "" || projectName === null) {
+        projectName = 'default';
+    }    
+
+    for (const project of projects) {
+        if (project.name === projectName) {
+            project.tasks.push(todo);
+        } 
+    }
+    console.log(projects);
+}
+
 function controlUserInterface() {
     const choice = Number(prompt('Choices:\n1. Add a to do'));
-    
+
     if (choice === 1) {
         const todo = makeTodo();
         for (const attr in todo) {
             todo[attr] = prompt(`Enter ${attr}`);
         }
-        console.log(todo);
+        const projectName = prompt('Enter project Name:');
+        storeProject({projectName, todo});
     }
 }
 
