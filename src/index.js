524d82586aa8880866d8decf0185ed62c99343a4
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-04-11 19:00:21 +0000

    Make project class
    Create project class to create new projects.
    Provide method to add tasks to project.
    Provide method to show tasks in project.
    Provide method to edit task state in project.

--- src/index.js
+++ src/index.js
@@ -21,3 +21,41 @@ class Task {
         return this.showTask() + `\n${this.description}`;
     }
 }
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+    addTask(title, dueDate, description) {
+        const task = new Task(title, dueDate, description)
+        this.tasks.push(task);
+    }
+    listTasks() {
+        return this.tasks.map((task, index) => `${index + 1}. ${task.showTask()}`);
+    }
+    showTaskDetails(index) {
+        let detail = "";
+        this.tasks.forEach((task, i) => {
+            if (i === index) {
+                detail = task.showDetail();
+            }
+        });
+        return detail;
+    }
+    toggleTaskCompleted(index) {
+        this.tasks.forEach((task, i) => {
+            if (i === index) { task.toggleCompleted(); }
+        });
+    }
+}
+
+const projectName = 'default';
+const project = new Project(projectName);
+
+project.addTask('Test','2025-04-12', 'Test #1');
+project.addTask('2nd test', '2025-05-12', 'Test #2');
+project.listTasks().forEach(taskString => console.log(taskString));
+project.toggleTaskCompleted(1);
+project.listTasks().forEach(taskString => console.log(taskString));
+console.log(project.showTaskDetails(0));
